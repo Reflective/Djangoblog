06ea3fb35cf130824573c519f2820dd3aa8eac03
Reflective <Reflective@users.noreply.github.com>
2022-07-28 02:03:32 -0400

    Added conditional titles to html template

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Jungle Wizard',
+        'title': 'Welcome to the Jungle',
+        'content': 'First post content',
+        'date_posted': 'July 27, 2022'         
+    },    
+    {
+        'author': 'Stinkman',
+        'title': 'I am sad',
+        'content': 'Deuces content',
+        'date_posted': 'July 28, 2022'         
+    },
+    
+]
+
+
+
 # Create your views here. 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request,'blog/about.html', {'title': 'About'})
