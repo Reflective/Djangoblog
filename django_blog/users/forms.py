b91218981f6634bcc95f502bb015c7e6086963f2
Reflective <Reflective@users.noreply.github.com>
2022-07-28 21:27:54 -0400

    Implemented image resizing and added pics to blog

--- django_blog/users/forms.py
+++ django_blog/users/forms.py
@@ -2,6 +2,7 @@ from socket import fromshare
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,4 +10,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
         
