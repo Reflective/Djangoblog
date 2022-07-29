3f806f61e0ce571930c77b198032747d3b3fc0c2
Reflective <Reflective@users.noreply.github.com>
2022-07-29 18:35:44 -0400

    Working on deploy

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -24,12 +24,18 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t--$e_5qvntez4l4ax5x4jb_3xkny2elmx8ew*2z5#@l6d8+%h"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "brandonpoll.com",
+    "www.brandonpoll.com",
+    "144.126.139.242",
+    "localhost",
+    "0.0.0.0",
+]
 
 
 # Application definition
